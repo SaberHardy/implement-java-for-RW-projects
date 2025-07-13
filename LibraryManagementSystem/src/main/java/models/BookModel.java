36687ac2969815614fbfd6e3b60a1dc7164b5242
Saber <samirsamado5@gmail.com>
2025-07-13 15:36:57 -0700

    create book model

---
+++ LibraryManagementSystem/src/main/java/models/BookModel.java
@@ -0,0 +1,58 @@
+package main.java.models;
+
+import java.util.Date;
+
+public class BookModel {
+    String bookName;
+    String bookId;
+    String bookAuthor;
+    Date bookBorrowedDay;
+    int bookQuantity;
+
+    public BookModel(String bookName, String bookId, String bookAuthor, Date bookBorrowedDay, int bookQuantity) {
+        if (!bookName.isEmpty()) {
+            this.bookName = bookName;
+        } else {
+            throw new IllegalArgumentException("The book name should be provided");
+        }
+        this.bookId = bookId;
+        if (!bookAuthor.isEmpty()) {
+            this.bookAuthor = bookAuthor;
+        } else {
+            throw new IllegalArgumentException("The Author name should be provided");
+        }
+        this.bookBorrowedDay = bookBorrowedDay;
+        if (bookQuantity > 0) this.bookQuantity = bookQuantity;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public Date getBookBorrowedDay() {
+        return bookBorrowedDay;
+    }
+
+    public int getBookQuantity(int bookQuantity) {
+        if (bookQuantity < 0) {
+            throw new IllegalArgumentException("The quantity should be greater than Zero!");
+        } else {
+            return bookQuantity;
+        }
+    }
+
+    public int updateBookQuantity(int bookQuantityToTake) {
+        if (bookQuantityToTake > 0 && bookQuantity > bookQuantityToTake) {
+            bookQuantity -= bookQuantityToTake;
+        }
+        return bookQuantity;
+    }
+}
