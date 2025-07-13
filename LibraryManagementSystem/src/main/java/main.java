8c8ff9a9e5301a2724d04ed2e31b9b4d760c9339
Saber <samirsamado5@gmail.com>
2025-07-13 15:36:46 -0700

    add description to the main

--- LibraryManagementSystem/src/main/java/main.java
+++ LibraryManagementSystem/src/main/java/main.java
@@ -1,5 +1,12 @@
 package main.java;
 
+/**
+ * Concepts Practiced: Composition, Streams, Lambda
+ * 		○ Book borrowing/returning
+ * 		○ Search books (by author/title)
+ * 		○ Calculate late fees (use LocalDate)
+ *      ○ Serialize data with JSON (advanced)
+ **/
 public class main {
     public static void main(String[] args) {
 
